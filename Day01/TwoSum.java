8e86b357e70bd59d2345bca2012a174c9ed1f174
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-07-16 18:26:51 +0530

    Update TwoSum.java

--- Day01/TwoSum.java
+++ Day01/TwoSum.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int n = nums.length;
+       for(int i =0;i<n ;i++){
+        for(int j=n-1;j>=0;j--){
+            if(nums[i]+nums[j]==target && i!=j){
+                return new int[]{i,j};
+            }
+        }
+       }
+       return new int[]{-1,-1};
+    }
+}
