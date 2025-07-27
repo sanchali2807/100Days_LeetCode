b6da3f303fe76cb5d55b73ee1022b18c79e384ad
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-07-27 19:39:28 +0530

    Create MergeSortedArray.java

---
+++ DAY12/MergeSortedArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0 ;i<nums2.length ;i++){
+            nums1[m++] = nums2[i];
+        }
+        
+        Arrays.sort(nums1);
+
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
+    }
+
+}
