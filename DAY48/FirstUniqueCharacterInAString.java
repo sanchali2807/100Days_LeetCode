143684838160f22290a1eb17cce2cb6923d8087c
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-09-01 21:44:30 +0530

    Create FirstUniqueCharacterInAString.java

---
+++ DAY48/FirstUniqueCharacterInAString.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> mp = new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(mp.containsKey(s.charAt(i))){
+                int freq = mp.get(s.charAt(i));
+                mp.put(s.charAt(i),freq+1);
+            }else{
+                mp.put(s.charAt(i),1);
+            }
+        }
+          for (int i = 0; i < s.length(); i++) {
+            if (mp.get(s.charAt(i)) == 1) {
+                return i;  
+            }
+        }
+
+        return -1; 
+    
+        }
+    
+}
