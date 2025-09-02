a6276bb33d8f732c1984fdde113c043ba2f80cd8
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-09-02 20:39:46 +0530

    Create WordBreak.java

---
+++ DAY49/WordBreak.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>();
+        for(String str : wordDict){
+            words.add(str);
+        }
+        boolean [] dp = new boolean [s.length()+1];
+        dp[0] = true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && words.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+            return dp[s.length()];
+        
+    }
+}
