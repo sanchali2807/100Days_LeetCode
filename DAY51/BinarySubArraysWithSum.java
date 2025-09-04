0938218f47f6cb37facdcf344bf8c6fbbd6b85c5
Sanchali <156992815+sanchali2807@users.noreply.github.com>
2025-09-04 20:06:08 +0530

    Create BinarySubArraysWithSum.java

---
+++ DAY51/BinarySubArraysWithSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+       Map<Integer,Integer> mp = new HashMap<>();
+
+       int sum = 0;
+       int count = 0;
+       mp.put(0,1);
+       for(int i=0;i<nums.length;i++){
+        sum += nums[i];
+        if(mp.containsKey(sum-goal)){
+            count+=mp.get(sum-goal);
+        }
+         mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+       }
+       return count;
+    }
+}
