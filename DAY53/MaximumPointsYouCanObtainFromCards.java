7e04c8e1ad224d8161f6c4cb062806e2d8df08ea
Sanchali <sanchali280705@gmail.com>
2025-09-06 20:05:22 +0530

    Create MaximumPointsYouCanObtainFromCards.java

---
+++ DAY53/MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+
+
+        int leftSum = 0;
+        for (int i = 0; i < k; i++) {
+            leftSum += cardPoints[i];
+        }
+
+        int maxScore = leftSum;
+
+        int rightSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            leftSum -= cardPoints[k - 1 - i];       
+            rightSum += cardPoints[n - 1 - i];     
+            maxScore = Math.max(maxScore, leftSum + rightSum);
+        }
+
+        return maxScore;
+    }
+}
